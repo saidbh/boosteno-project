d8c10ec45a735c7f2816ed3682898164992ea3d5
saidbh <saidbenhmed3@gmail.com>
2021-11-16 11:46:07 +0100

    boosteno

--- resources/views/academy/classes/index.blade.php
+++ resources/views/academy/classes/index.blade.php
@@ -188,12 +188,11 @@
                             @csrf
                             <div class="form-row">
                                 <div class="form-group col-md-6">
-                                    <label for="departement">Département</label>
+                                    <label for="departement">Enseignants</label>
                                     <select name="departement" id="departement" class="form-control" required>
-                                        <option value="">Veillez choisir un département </option>
-                                        @foreach ($departments as $department)
-                                        <option value="{{ $department->id }}">{{ $department->name }}</option>
-                                        @endforeach
+                                        <option value="">Veillez choisir un Enseignants </option>
+                                        <option value="teacher1">enseignats 1 </option>
+                                        <option value="teacher2">enseignats 2 </option>
                                     </select>
                                 </div>
                                 <div class="form-group col-md-6">
